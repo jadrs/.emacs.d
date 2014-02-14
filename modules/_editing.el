17fe19644ec12e6f853ef979f5326b212096f7e0
JSanchez <jrg.sanchez@gmail.com>
2014-02-14 08:25:19 -0300

    tabbar

--- modules/_editing.el
+++ modules/_editing.el
@@ -75,24 +75,6 @@
 (global-set-key (kbd "C-M-k")       'kill-this-buffer)
 (global-set-key (kbd "M-=")         'hippie-expand) ; Autocompletion
 
-(require 'tabbar)
-(tabbar-mode t)
-(global-set-key [M-S-left] 'tabbar-backward-tab)
-(global-set-key [M-S-right] 'tabbar-forward-tab)
-(global-set-key [M-S-up] 'tabbar-forward-group)
-(global-set-key [M-S-down] 'tabbar-backward-group)
-
- ;; customize to show all normal files in one group
-(defun my-tabbar-buffer-groups()
-  "Returns the name of the tab group names the current buffer belongs to.
- There are two groups: Emacs buffers (those whose name starts
- with '*', plus dired buffers), and the rest.  This works at
- least with Emacs v24.2 using tabbar.el v1.7."
-  (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-	      ((eq major-mode 'dired-mode) "emacs")
-	      (t "user"))))
-(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
-
 ;; snippets
 (require 'yasnippet)
 ;; (setq yas-snippets-dir "~/.emacs.d/snippets")  ; override default snippets
--- modules/_ui.el
+++ modules/_ui.el
@@ -64,3 +64,32 @@
 
 ; copy/paste
 (setq x-select-enable-clipboard t)
+
+; tabs for buffers
+(require 'tabbar)
+
+(global-set-key [M-S-left] 'tabbar-backward-tab)
+(global-set-key [M-S-right] 'tabbar-forward-tab)
+(global-set-key [M-S-up] 'tabbar-forward-group)
+(global-set-key [M-S-down] 'tabbar-backward-group)
+
+ ;; customize to show all normal files in one group
+(defun my-tabbar-buffer-groups()
+  "Returns the name of the tab group names the current buffer belongs to.
+ There are two groups: Emacs buffers (those whose name starts
+ with '*', plus dired buffers), and the rest.  This works at
+ least with Emacs v24.2 using tabbar.el v1.7."
+  (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+	      ((eq major-mode 'dired-mode) "emacs")
+	      (t "user"))))
+(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
+
+(set-face-attribute 'tabbar-selected nil
+		    :background "gray75"
+		    :foreground "black"
+		    :box '(:line-width 1 :color "gray75" :style nil))
+
+;; (custom-set-variables
+;; '(tabbar-separator (quote (0.5))))
+
+(tabbar-mode t)
